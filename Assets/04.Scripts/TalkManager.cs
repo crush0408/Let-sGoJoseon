3b9ee03fabcc7ab25bf36c68078233e2fe4c1f61
CRUSH04 <msng0408@naver.com>
2022-10-19 10:36:03 +0900

    폴더 재구성

---
+++ Assets/04.Scripts/TalkManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkManager : MonoBehaviour
+{
+    
+    public Text talkTest;
+    //public GameObject scanObj;
+    public List<string> textString = new List<string>(3);
+    public int key = 0;
+    public int maxKey;
+    public bool IsKing = false;
+    public Image kingImage;
+    public Image fourTimeImage;
+    public Image panelImage;
+    public Text whoText;
+    //private string replaceSTR;
+    void Awake()
+    {
+        panelImage.color = new Color(255,255,255,0);
+        talkTest.text = string.Format(" ");
+        whoText.text = string.Format(" ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(textString != null && Input.GetMouseButtonUp(1) && key < maxKey){
+            panelImage.color = new Color(255,255,255,255);
+
+            if(textString[key].Contains("정조"))
+            {
+                whoText.text = string.Format("정조요!");
+                RemoveName(key, "- 정조");
+                fourTimeImage.rectTransform.localScale = new Vector3(1f,1f,1f);
+                kingImage.rectTransform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            }
+            else if(textString[key].Contains("내관")){
+                whoText.text = string.Format("내관이요!");
+                RemoveName(key,"- 내관");
+                kingImage.rectTransform.localScale = new Vector3(1f,1f,1f);
+                fourTimeImage.rectTransform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            }
+            talkTest.text = string.Format(" ");
+            //whoText.text = string.Format("누구인가?");
+            talkTest.text = textString[key];
+            key++;
+            Debug.Log(key);
+            
+            //kingImage.rectTransform.localScale = new Vector3(1f,1f,1f);
+            //fourTimeImage.rectTransform.localScale = new Vector3(1f,1f,1f);
+
+        }
+    }
+    void RemoveName(int key,string n){
+        string sPath = textString[key];
+        int iIndex = sPath.LastIndexOf(n);
+        if (iIndex > -1)                
+            sPath = sPath.Remove( iIndex );
+        //Debug.Log( sPath );
+        textString[key] = sPath;
+    }
+}
---
+++ Assets/04.Scripts/Wang2.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wang2 : MonoBehaviour
+{
+    public List<string> s = new List<string>();
+    public TalkManager talkManager;
+    void Start()
+    {
+        
+        s[0] = "조사 다녀 왔습니다 전하 - 내관";
+        s[1] = "그래 수고했다. 백성의 생각은 어떠해 보이더냐? - 정조" ;
+        s[2] = "저잣거리에서 이 근방에서 가장 나이가 많은 할아버지를 만났습니다 - 내관" ;
+        s[3] = "가장 나이가 많은 할아버지라니 꽤 운이 좋았구나 - 정조";
+        s[4] = "맞습니다. 하지만 놀란 건 따로 있습니다 - 내관";
+        s[5] = "더 놀라운 것 이라니, 그게 무엇이냐 - 정조";
+        s[6] = "지금 백성들은 너무도 순수한 마음으로 전하의 정책을 기다리고 있습니다. - 내관";
+        s[7] = "또한 일편단심의 마음으로 전하를 신뢰하고 있습니다. - 내관";
+        s[8] = "하하하 내가 참 유복하구나 - 정조";
+        s[9] = "근래에 쌓였던 답답함이 한번에 내려가는 것 같구나 - 정조";
+        s[10] = "나를 신뢰하는 백성들에게 실망을 안겨주지는 않겠다 - 정조";
+        s[11] = "좋다. 수원화성을 짓겠다. - 정조";
+        s[12] = "정약용에게 수원화성 건축을 맡겨라 - 정조";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            Debug.Log("input data");
+            for(int i = 0;i < s.Count;i ++){
+                talkManager.textString[i] = s[i];
+                
+            }
+                talkManager.maxKey = s.Count;
+
+        }
+    }
+}
